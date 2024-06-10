e7a34149de13e2b73a1e2dd368a4d5bb7e1a7b46
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-10 12:09:38 +0530

    finish section url params in loader

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 import Root, {
   action as rootAction,
   loader as rootLoader,
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
