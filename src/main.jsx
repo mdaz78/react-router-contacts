aa6c76e7c0afedba4ec9318527b9319817191ef1
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-11 12:46:21 +0530

    finish section "JSX Routes"

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,45 @@ import Root, {
   loader as rootLoader,
 } from "./routes/root";
 
+// JSX way of creating a route
+// import {
+//   createRoutesFromElements,
+//   createBrowserRouter,
+//   Route,
+// } from "react-router-dom";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route
+//       path="/"
+//       element={<Root />}
+//       loader={rootLoader}
+//       action={rootAction}
+//       errorElement={<ErrorPage />}
+//     >
+//       <Route errorElement={<ErrorPage />}>
+//         <Route index element={<Index />} />
+//         <Route
+//           path="contacts/:contactId"
+//           element={<Contact />}
+//           loader={contactLoader}
+//           action={contactAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/edit"
+//           element={<EditContact />}
+//           loader={contactLoader}
+//           action={editAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/destroy"
+//           action={destroyAction}
+//         />
+//       </Route>
+//     </Route>
+//   )
+// );
+
 const router = createBrowserRouter([
   {
     path: "/",
